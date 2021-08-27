a157da982d00d5317a5f00997242caff9fa56e7a
Magicoo51889 <jackot11f@gmail.com>
2021-08-27 20:49:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-const Discord = require('discord.js');
 import { Message } from "discord.js";
 import { validateEnv} from "./utils/validateEnv";
 import { onMessage } from "./events/onMessage";
 
+const { Client, Intents } = require('discord.js');
+
 (async () => {
     if (!validateEnv()) return;
 
-    const BOT = new Discord.Client();
+    const BOT = new Client();
 
     BOT.on("ready", () => console.log("Connected to Discord"));
     BOT.on("message", async (message: Message) => await onMessage(message));
