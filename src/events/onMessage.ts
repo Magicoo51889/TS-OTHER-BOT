769b5505f14b0f8d1d5bb4f3dd23406acc58589b
Magicoo51889 <jackot11f@gmail.com>
2022-04-26 22:02:08 +0100

    npm install & update to onMessage

--- src/events/onMessage.ts
+++ src/events/onMessage.ts
@@ -3,8 +3,7 @@ import { Message } from "discord.js";
 export const onMessage = async (message: Message) => {
     console.log(message.content)
 
-    const prefix = "[BOT]"
-    if (!message.content.startsWith(prefix))
+    if (!message.content.startsWith("*"))
 
     if (message.author.bot) {
         return;
